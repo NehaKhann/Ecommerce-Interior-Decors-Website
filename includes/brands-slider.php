6c993888c02bfd77432f7433dca973492bb8f6b5
Neha Khan <n.nehakhan333@gmail.com>
2020-08-19 20:26:15 +0500

    Its Ready

---
+++ includes/brands-slider.php
@@ -0,0 +1,18 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brand</h3>
+		<div class="logo-slider-inner">
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+
+						<img data-echo="brandsimage/tb2.jpg" src="brandsimage/tb2.jpg" alt="" width="1150" height="200">
+
+				</div><!--/.item-->
+
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+
+</div><!-- /.logo-slider -->
